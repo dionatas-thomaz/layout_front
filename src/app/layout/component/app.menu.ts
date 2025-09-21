98b24d38cc013f96b71286984958517122684556
dionatas_thomaz <dionatassanto0@gmail.com>
2025-09-21 17:45:40 -0300

    refactor: remocao de modulos

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -1,156 +1,96 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { MenuItem } from 'primeng/api';
+import { Router, RouterModule } from '@angular/router';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenuItem, MessageService } from 'primeng/api';
 import { AppMenuitem } from './app.menuitem';
+
+import { Toast } from 'primeng/toast';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
-    imports: [CommonModule, AppMenuitem, RouterModule],
-    template: `<ul class="layout-menu">
-        <ng-container *ngFor="let item of model; let i = index">
-            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
-            <li *ngIf="item.separator" class="menu-separator"></li>
-        </ng-container>
-    </ul> `
+    imports: [CommonModule, AppMenuitem, RouterModule, Toast],
+    providers: [DialogService, MessageService],
+    template: `
+        <p-toast />
+        <ul class="layout-menu">
+            <ng-container *ngFor="let item of model; let i = index">
+                <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+                <li *ngIf="item.separator" class="menu-separator"></li>
+            </ng-container>
+        </ul>
+    `
 })
 export class AppMenu {
     model: MenuItem[] = [];
+    hasStock = false;
+    hasService = false;
+    hasFinance = false;
+    ref: DynamicDialogRef | undefined;
+
+    constructor(
+      //  private tokenService: TokenService,
+        private router: Router,
+        public dialogService: DialogService,
+        public messageService: MessageService
+    ) {
+        // this.hasStock = tokenService.hasStock();
+        // this.hasService = tokenService.hasService();
+        // this.hasFinance = tokenService.hasFinance();
+    }
+
+    logout() {
+       // this.tokenService.remove();
+        this.router.navigate(['/auth/login']);
+    }
 
     ngOnInit() {
         this.model = [
             {
-                label: 'Home',
+                                label: 'Home',
                 items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
+           
+                // label: 'Estoque',
+                // visible: this.hasStock,
+                // items: [
+                //     { label: 'Vendas', icon: 'pi pi-shopping-cart', routerLink: ['/estoque/vendas'] },
+                //     { label: 'Produtos', icon: 'pi pi-tag', routerLink: ['/estoque/produtos'] }
+                // ]
             },
             {
-                label: 'UI Components',
+                // label: 'Serviço',
+                // visible: this.hasService,
+                // items: [{ label: 'Serviço', icon: 'pi pi-warehouse', routerLink: ['/servico/servicos'] }]
+            },
+            {
+                // label: 'Financeiro',
+                // icon: 'pi pi-fw pi-briefcase',
+                // visible: this.hasFinance,
+                // items: [
+                //     { label: 'Contas a pagar', icon: 'pi pi-money-bill', routerLink: ['/financeiro/contas-pagar'] },
+                //     { label: 'Contas a receber', icon: 'pi pi-wallet', routerLink: ['/financeiro/contas-receber'] }
+                // ]
+            },
+            {
+                label: 'Útilitários',
                 items: [
-                    { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
-                    { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
-                    { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
-                    { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
-                    { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
-                    { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
-                    { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
-                    { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
-                    { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
-                    { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
-                    { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
-                    { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
-                    { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
-                    { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
-                    { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
+                    // {
+                    //     label: 'Configurações',
+                    //     icon: 'pi pi-cog',
+                    //     items: [
+                    //         {
+                    //             label: 'Dashboard',
+                    //             icon: 'pi pi-chart-pie',
+                    //             routerLink: ['/configuracao/graficos']
+                    //         }
+                    //     ]
+                    // },
                 ]
             },
             {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                routerLink: ['/pages'],
-                items: [
-                    {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: ['/pages/crud']
-                    },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/pages/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    }
-                ]
-            },
-            {
-                label: 'Hierarchy',
-                items: [
-                    {
-                        label: 'Submenu 1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 1.1',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                            {
-                                label: 'Submenu 1.2',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 2.1',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                            {
-                                label: 'Submenu 2.2',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Get Started',
-                items: [
-                    {
-                        label: 'Documentation',
-                        icon: 'pi pi-fw pi-book',
-                        routerLink: ['/documentation']
-                    },
-                    {
-                        label: 'View Source',
-                        icon: 'pi pi-fw pi-github',
-                        url: 'https://github.com/primefaces/sakai-ng',
-                        target: '_blank'
-                    }
-                ]
+                label: 'Sair',
+                items: [{ label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout() }]
             }
         ];
     }
